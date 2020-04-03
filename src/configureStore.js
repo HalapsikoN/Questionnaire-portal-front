607ce5de820bfb156810795c40cc4eec12100403
Dmitry Alhovik <33232288+HalapsikoN@users.noreply.github.com>
2020-04-03 23:52:52 +0300

    Revert "logIN/signUP"

--- src/configureStore.js
+++
@@ -1,9 +0,0 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {userReducer} from "./store/reducer";
-import thunkMiddleware from "redux-thunk"
-
-export const rootReducer = combineReducers({
-    user: userReducer
-});
-
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
